ee85373fafc1c730619c6477c567660ea826c483
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-11-12 17:56:22 -0600

    Resolving with data

--- schema/schema.js
+++ schema/schema.js
@@ -1,8 +1,11 @@
 const graphql = require('graphql');
+const _ = require('lodash');
+
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema
 } = graphql;
 
 const UserType = new GraphQLObjectType({
@@ -12,4 +15,26 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   }
+});
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id:'47', firstName: 'Samantha', age: 21 }
+];
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id })
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
