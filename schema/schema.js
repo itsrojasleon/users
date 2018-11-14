533daf5a74cb86bd55b4fd07c7aff3a649952293
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-11-14 15:54:16 -0600

    Nodemon setup

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 
 const {
   GraphQLObjectType,
@@ -17,11 +17,6 @@ const UserType = new GraphQLObjectType({
   }
 });
 
-const users = [
-  { id: "23", firstName: 'Bill', age: 20 },
-  { id: "47", firstName: 'Samantha', age: 21 }
-];
-
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -29,7 +24,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id })
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then((res) => res.data);
       }
     }
   }
