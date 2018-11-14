a448dab595fd2aef2cafa50960ee417907250677
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-11-13 20:08:57 -0600

    A realistic data source

--- schema/schema.js
+++ schema/schema.js
@@ -18,8 +18,8 @@ const UserType = new GraphQLObjectType({
 });
 
 const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id:'47', firstName: 'Samantha', age: 21 }
+  { id: "23", firstName: 'Bill', age: 20 },
+  { id: "47", firstName: 'Samantha', age: 21 }
 ];
 
 const RootQuery = new GraphQLObjectType({
